2c7ce15e365d823132ad716d3bdbf7232dfa6c8d
dmasieh <dmasieh@gmail.com>
2020-01-18 18:24:36 -0500

    courseinfo Step 3: modified variable definitions so that instead of passing 6 redundant constants in the app component, we'll be passing in 3 object const variables with two key-value pairs each

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -3,71 +3,48 @@ import ReactDOM from 'react-dom'
 
 const Header = (props) => {
 	console.log(props)
-    return (
-        <div>
-            <h1>{props.course}</h1>
-        </div>
-    )
+    return <h1>{props.course}</h1>
 }
 
 const Part = (props) => {
     console.log(props)
-    return (
-        <p>
-            Part {props.exerciseNum}: {props.exerciseName}, {props.exerciseCount} exercises
-        </p>
-    )
+    return <p>Part {props.exerciseNum}: {props.exerciseName}, {props.exerciseCount} exercises</p>
 }
 
 const Content = (props) => {
 	console.log(props)
 	return (
-		<div>
+		<>
             <Part exerciseNum='1' exerciseName={props.part1} exerciseCount={props.exercises1} />
             <Part exerciseNum='2' exerciseName={props.part2} exerciseCount={props.exercises2} />
             <Part exerciseNum='3' exerciseName={props.part3} exerciseCount={props.exercises3} />
-        </div>
+        </>
 	)
 }
 
 
 const Total = (props) => {
     console.log(props)
-    return(
-        <div>
-            <p>Number of exercises: {props.exercises1 + props.exercises2 + props.exercises3}</p>
-        </div>
-    )
+    return <p>Number of exercises: {props.exercises1 + props.exercises2 + props.exercises3}</p>
 }
 
 
 const App = () => {
     const course = 'Half Stack application development'
-    const part1 = 'Fundamentals of React'
-    const exercises1 = 10
-    const part2 = 'Using props to pass data'
-    const exercises2 = 7
-    const part3 = 'State of a component'
-    const exercises3 = 14
+    const part1 = { name:    'Fundamentals of React', exercises: 10 }
+    const part2 = { name: 'Using props to pass data', exercises:  7 }
+    const part3 = { name:     'State of a component', exercises: 14 }
 
     return (
         <div>
         	<Header course={course}/>
             <Content 
-                part1={part1} 
-                part2={part2}
-                part3={part3}   
-                exercises1={exercises1}  
-                exercises2={exercises2}  
-                exercises3={exercises3}  
+                part1={part1.name} part2={part2.name} part3={part3.name}   
+                exercises1={part1.exercises} exercises2={part2.exercises} exercises3={part3.exercises}  
             />
-
             <Total 
-                exercises1={exercises1}  
-                exercises2={exercises2}  
-                exercises3={exercises3}
+                exercises1={part1.exercises} exercises2={part2.exercises} exercises3={part3.exercises}
             />
-
         </div>
     )
 }
