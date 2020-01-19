8203e4cf4b46c58c7b47b68180f22753d1a9a163
dmasieh <dmasieh@gmail.com>
2020-01-19 12:42:24 -0500

    unicafe Step 3: Refactored application to define a separate Statistics component to be called into the main App component

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -1,16 +1,29 @@
 import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
-// Refactored button into a component to be 
-// called later, as done earlier in the lesson
+// Refactored button into a component to be called later
 const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>
+
+// create a separate component for the statistics
+const Statistics = ({ good, bad, neutral }) => {
+    const total = good + bad + neutral
+    return (
+        <>
+            <p>Good votes: {good}</p>
+            <p>Neutral votes: {neutral}</p>
+            <p>Bad votes: {bad}</p>
+            <p>All Votes: {total}</p>
+            <p>Average Votes: {(good + bad * -1) / total}</p>
+            <p>Percent Positive: {(good / total) * 100}%</p>
+        </>
+    )
+}
 
 const App = () => {
   	// save clicks of each button to own state
   	const [good, setGood] = useState(0)
   	const [neutral, setNeutral] = useState(0)
     const [bad, setBad] = useState(0)
-    const total = good + bad + neutral  
 
   	return (
     	<div>
@@ -19,12 +32,7 @@ const App = () => {
 			<Button text='Neutral' onClick={() => setNeutral(neutral+ 1)} />
 			<Button text='Bad' onClick={() => setBad(bad + 1)} />
 			<h3>statistics</h3>		    		
-			<p>Good votes: {good}</p>
-			<p>Neutral votes: {neutral}</p>
-			<p>Bad votes: {bad}</p>
-            <p>All Votes: {total}</p>
-            <p>Average Votes: {(good + bad * -1) / total}</p>
-            <p>Percent Positive: {(good / total) * 100}%</p>
+			<Statistics good={good} bad={bad} neutral={neutral} />
 		</div>
   	)
 }
