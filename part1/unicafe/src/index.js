7eb8d41f7e815fbdafbf6f6d763c7b0b72fa79be
dmasieh <dmasieh@gmail.com>
2020-01-19 13:01:45 -0500

    unicafe Step 4: changed application to display statistics only if feedback has been gathered

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -7,16 +7,29 @@ const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>
 // create a separate component for the statistics
 const Statistics = ({ good, bad, neutral }) => {
     const total = good + bad + neutral
-    return (
-        <>
-            <p>Good votes: {good}</p>
-            <p>Neutral votes: {neutral}</p>
-            <p>Bad votes: {bad}</p>
-            <p>All Votes: {total}</p>
-            <p>Average Votes: {(good + bad * -1) / total}</p>
-            <p>Percent Positive: {(good / total) * 100}%</p>
-        </>
-    )
+
+    // conditionally display statistics based on feedback given
+    if(good >= 1 && (neutral + bad === 0)){
+        return (
+            <>	
+				<p>Good Votes: {good}</p>
+				<p>So far, there's only Good Votes, a vote average of 1 and Positive Feedback!</p>
+            </>
+        )
+    } else if(total === 0) {
+        return <h1>No Feedback Given Yet</h1>
+    } else {
+        return (
+            <>
+                <p>Good votes: {good}</p>
+                <p>Neutral votes: {neutral}</p>
+                <p>Bad votes: {bad}</p>
+                <p>All Votes: {total}</p>
+                <p>Average Votes: {(good + bad * -1) / total}</p>
+                <p>Percent Positive: {(good / total) * 100}%</p>
+            </>
+        )
+    }
 }
 
 const App = () => {
@@ -31,7 +44,7 @@ const App = () => {
 			<Button text='Good' onClick={() => setGood(good + 1)} />
 			<Button text='Neutral' onClick={() => setNeutral(neutral+ 1)} />
 			<Button text='Bad' onClick={() => setBad(bad + 1)} />
-			<h3>statistics</h3>		    		
+			<h3>Vote Statistics!</h3>		    		
 			<Statistics good={good} bad={bad} neutral={neutral} />
 		</div>
   	)
